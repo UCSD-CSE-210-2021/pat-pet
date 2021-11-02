3b3f0a18cab122c3785dab6c5d3cbc1a8e8d9f3c
cosmobiosis <cosmobiosis@gmail.com>
2021-11-01 23:34:36 -0700

    feat: grid of cards

--- frontend/src/pages/Search/index.js
+++ frontend/src/pages/Search/index.js
@@ -1,10 +1,14 @@
+
+import "./index.css";
 import React, { useState } from "react";
-import { Input } from "antd";
+import { Input, Card, Col, Row } from "antd";
 import { sendPostRequest } from "../../utils/request";
 const { Search } = Input;
+const { Meta } = Card;
 
 export default function SearchPage() {
   const [pets, setPets] = useState([]);
+  
   const onSearch = async (keyword) => {
     try {
       console.log(process.env.BACKEND_ENDPOINT)
@@ -21,23 +25,28 @@ export default function SearchPage() {
   }
 
   return (
-    <>
-      <Search placeholder="keyword" onSearch={onSearch} enterButton />
-      <div style={{display: "flex", flexDirection: "row"}}>
+    <div className="search-page">
+      <Search style={{ width: 400, marginBottom: 20 }} placeholder="keyword" onSearch={onSearch} enterButton />
+      <Row gutter={[16, 16]} style={{height: window.innerHeight - 100, overflowX: "none", overflowY: "scroll"}}>
         {pets.map((pet, i) => 
         (
-          <div key={i} 
-            style={{display: "flex", flexDirection: "column"}}>
-            <span>
-              {pet.name}
-            </span>
-            <span>
-              {pet.description}
-            </span>
-            <img style={{width: 150, height: 150, objectFit: "scale-down"}} alt="petimg" src={pet.image_url}/>
-          </div>
+          <>
+            <Col style={{marginRight: 30, marginBottom: 20}}>
+              <Card
+                key={i}
+                bordered={false}
+                hoverable
+                style={{ width: 250, height: 320 }}
+                cover={ <img style={{width: 150, height: 150, objectFit: "scale-down"}} alt="petimg" src={pet.image_url}/>}
+              >
+                <Meta 
+                  title={<span className="cute-font-title">{pet.name}</span>} 
+                  description={<span className="cute-font-body">{pet.description}</span>} />
+              </Card>
+            </Col>
+          </>
         ))}
-    </div>
-  </>
+      </Row>
+  </div>
   );
 }
