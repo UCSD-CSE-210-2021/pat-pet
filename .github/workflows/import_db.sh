af3d275b774b5492157df5fcd12ab680a56b983c
lyw07 <lywang07@gmail.com>
2021-11-21 01:16:04 -0800

    set correct sql file address

--- .github/workflows/import_db.sh
+++ .github/workflows/import_db.sh
@@ -1,2 +1,2 @@
 PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT -c "create database patpet;"
-PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT -d patpet -f ../../backend/testdata/test.sql
+PGPASSWORD=$POSTGRES_PASSWORD psql -h $POSTGRES_HOST -U $POSTGRES_USER -p $POSTGRES_PORT -d patpet -f ./testdata/test.sql
